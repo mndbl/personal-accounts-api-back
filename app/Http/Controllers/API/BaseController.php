427a330875268bb3c800ee218efde22aaa41b37a
José Mendible <66981978+mndbl@users.noreply.github.com>
2023-01-21 12:06:07 -0700

    correction errores

--- app/Http/Controllers/API/BaseController.php
+++ app/Http/Controllers/API/BaseController.php
@@ -9,24 +9,24 @@ class BaseController extends Controller
 {
     public function sendResponse($result, $message)
     {
-        $response=[
-            'success'=>true,
-            'data'=> $result,
-            'message'=>$message
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message
         ];
         return response()->json($response);
     }
 
-    public function sendError($error, $errorMessages=[],$code=404)
+    public function sendError($error, $errorMessages = [], $code = 404)
     {
-        $response=[
-            'success'=>false,
-            'message'=>$error
+        $response = [
+            'success' => false,
+            'message' => $error,
+            'errorMessages' => $errorMessages,
+            'data' => []
         ];
 
-        if (!empty($errorMessages)) {
-            $response['data']=$errorMessages;
-        }
-        return response()->json($response,$code);
+
+        return response()->json($response, $code);
     }
 }
